c7e7c83079d40c5076f45b6815e7857ded2c4975
Chris Titus <dfm.titus@gmail.com>
2022-10-24 09:27:32 -0500

    profile update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -187,3 +187,13 @@ function pgrep($name) {
 
 ## Final Line to set prompt
 oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 #If the file does not exist, create it.
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
      try {
-         $null = Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+         Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
          Write-Host "The profile @ [$PROFILE] has been created."
      }
      catch {
@@ -10,37 +10,19 @@ if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
  }
 # If the file already exists, show the message and do nothing.
  else {
-     Write-Host "Cannot create profile, one already exists @ $PROFILE"
+	 	 Remote-Item $PROFILE -Force
+		 Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
  }
 & $profile
 
 # OMP Install
 #
-$app = "JanDeDobbeleer.OhMyPosh"
-if ((exit [int] (winget list --name posh | Select-String -Pattern 'found')-eq 1)) {
-	winget install $app
-} 
-else {
-    Write-Host "OhMyPosh Already Installed!"
-}
-
+winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
 
 # Font Install
-$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-$null = Invoke-RestMethod https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip?WT.mc_id=-blog-scottha -o cove.zip
-Get-ChildItem -Path $Source -Include '*.ttf','*.ttc','*.otf' -Recurse | ForEach {
-	$Font = "$($_.Name)"
-	If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
-		# Copy font to local temporary folder
-        	Copy-Item $($_.FullName) -Destination $TempFolder
-        
-	        # Install font
-        	$Destination.CopyHere($Font,0x10)
-	}
-	else {
-		Write-Host "Font Already Installed"
-	}
-        # Delete temporary copy of font
-        Remove-Item $Font -Force
-}
+Invoke-RestMethod https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip?WT.mc_id=-blog-scottha -o cove.zip
 
+# Choco install
+#
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
