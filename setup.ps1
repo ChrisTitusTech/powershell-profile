1a7026c10bf020bf31295c570e646251d1b2fca5
solomoncyj <solomoncyj@hotmail.com>
2023-08-02 19:53:31 +0800

    set -o in line 26 of setup.ps1 to be -outfile because it is ambiguous in pwsh version 7

--- setup.ps1
+++ setup.ps1
@@ -23,7 +23,7 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
 # If the file already exists, show the message and do nothing.
  else {
 		 Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
-		 Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+		 Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
 		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
  }
 & $profile
