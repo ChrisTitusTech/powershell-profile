4a1787669b45ddf98f3982d53e58c2fb2042f972
Chris Titus <contact@christitus.com>
2023-09-13 11:05:25 -0500

    remove warnings

--- oldprofile.ps1
+++ oldprofile.ps1
@@ -13,6 +13,9 @@
 ### This is the default policy on Windows Server 2012 R2 and above for server Windows. For 
 ### more information about execution policies, run Get-Help about_Execution_Policies.
 
+# Import Terminal Icons
+Import-Module -Name Terminal-Icons
+
 # Find out if the current user identity is elevated (has admin rights)
 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal $identity
@@ -21,25 +24,24 @@ $isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administ
 # If so and the current host is a command line, then change to red color 
 # as warning to user that they are operating in an elevated context
 # Useful shortcuts for traversing directories
-function cd...  { cd ..\.. }
-function cd.... { cd ..\..\.. }
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
 
 # Compute file hashes - useful for checking successful downloads 
-function md5    { Get-FileHash -Algorithm MD5 $args }
-function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
 # Quick shortcut to start notepad
-function n      { notepad $args }
+function n { notepad $args }
 
 # Drive shortcuts
-function HKLM:  { Set-Location HKLM: }
-function HKCU:  { Set-Location HKCU: }
-function Env:   { Set-Location Env: }
+function HKLM: { Set-Location HKLM: }
+function HKCU: { Set-Location HKCU: }
+function Env: { Set-Location Env: }
 
 # Creates drive shortcut for Work Folders, if current user account is using it
-if (Test-Path "$env:USERPROFILE\Work Folders")
-{
+if (Test-Path "$env:USERPROFILE\Work Folders") {
     New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
     function Work: { Set-Location Work: }
 }
@@ -47,33 +49,24 @@ if (Test-Path "$env:USERPROFILE\Work Folders")
 # Set up command prompt and window title. Use UNIX-style convention for identifying 
 # whether user is elevated (root) or not. Window title shows current version of PowerShell
 # and appends [ADMIN] if appropriate for easy taskbar identification
-function prompt 
-{ 
-    if ($isAdmin) 
-    {
+function prompt { 
+    if ($isAdmin) {
         "[" + (Get-Location) + "] # " 
-    }
-    else 
-    {
+    } else {
         "[" + (Get-Location) + "] $ "
     }
 }
 
 $Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
-if ($isAdmin)
-{
+if ($isAdmin) {
     $Host.UI.RawUI.WindowTitle += " [ADMIN]"
 }
 
 # Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
-function dirs
-{
-    if ($args.Count -gt 0)
-    {
+function dirs {
+    if ($args.Count -gt 0) {
         Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
-    }
-    else
-    {
+    } else {
         Get-ChildItem -Recurse | Foreach-Object FullName
     }
 }
@@ -81,16 +74,12 @@ function dirs
 # Simple function to start a new elevated process. If arguments are supplied then 
 # a single command is started with admin rights; if not then a new admin instance
 # of PowerShell is started.
-function admin
-{
-    if ($args.Count -gt 0)
-    {   
-       $argList = "& '" + $args + "'"
-       Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
-    }
-    else
-    {
-       Start-Process "$psHome\powershell.exe" -Verb runAs
+function admin {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+    } else {
+        Start-Process "$psHome\powershell.exe" -Verb runAs
     }
 }
 
@@ -101,14 +90,10 @@ Set-Alias -Name sudo -Value admin
 
 
 # Make it easy to edit this profile once it's installed
-function Edit-Profile
-{
-    if ($host.Name -match "ise")
-    {
+function Edit-Profile {
+    if ($host.Name -match "ise") {
         $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
-    }
-    else
-    {
+    } else {
         notepad $profile.CurrentUserAllHosts
     }
 }
@@ -117,73 +102,120 @@ function Edit-Profile
 # Delete them to prevent cluttering up the user profile. 
 Remove-Variable identity
 Remove-Variable principal
+
+Function Test-CommandExists {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'SilentlyContinue'
+    try { if (Get-Command $command) { RETURN $true } }
+    Catch { Write-Host "$command does not exist"; RETURN $false }
+    Finally { $ErrorActionPreference = $oldPreference }
+} 
 #
 # Aliases
 #
-New-Alias vim nvim
+# If your favorite editor is not here, add an elseif and ensure that the directory it is installed in exists in your $env:Path
+#
+if (Test-CommandExists nvim) {
+    $EDITOR='nvim'
+} elseif (Test-CommandExists pvim) {
+    $EDITOR='pvim'
+} elseif (Test-CommandExists vim) {
+    $EDITOR='vim'
+} elseif (Test-CommandExists vi) {
+    $EDITOR='vi'
+} elseif (Test-CommandExists code) {
+    $EDITOR='code'
+} elseif (Test-CommandExists notepad) {
+    $EDITOR='notepad'
+} elseif (Test-CommandExists notepad++) {
+    $EDITOR='notepad++'
+} elseif (Test-CommandExists sublime_text) {
+    $EDITOR='sublime_text'
+}
+Set-Alias -Name vim -Value $EDITOR
+
+
 function ll { Get-ChildItem -Path $pwd -File }
-function g { cd $HOME\Documents\Github }
-function gcom
-{
-	git add .
-	git commit -m "$args"
+function g { Set-Location $HOME\Documents\Github }
+function gcom {
+    git add .
+    git commit -m "$args"
 }
-function lazyg
-{
-	git add .
-	git commit -m "$args"
-	git push
+function lazyg {
+    git add .
+    git commit -m "$args"
+    git push
 }
-Function Get-PubIP {
- (Invoke-WebRequest http://ifconfig.me/ip ).Content
+function Get-PubIP {
+    (Invoke-WebRequest http://ifconfig.me/ip ).Content
 }
 function uptime {
-        Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';
-        EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+    #Windows Powershell only
+	If ($PSVersionTable.PSVersion.Major -eq 5 ) {
+		Get-WmiObject win32_operatingsystem |
+        Select-Object @{EXPRESSION={ $_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+	} Else {
+        net statistics workstation | Select-String "since" | foreach-object {$_.ToString().Replace('Statistics since ', '')}
+    }
 }
+
 function reload-profile {
-        & $profile
+    & $profile
 }
 function find-file($name) {
-        ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
-                $place_path = $_.directory
-                echo "${place_path}\${_}"
-        }
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
 }
 function unzip ($file) {
-        echo("Extracting", $file, "to", $pwd)
-	$fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object{$_.FullName}
-        Expand-Archive -Path $fullFile -DestinationPath $pwd
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+function ix ($file) {
+    curl.exe -F "f:1=@$file" ix.io
 }
 function grep($regex, $dir) {
-        if ( $dir ) {
-                ls $dir | select-string $regex
-                return
-        }
-        $input | select-string $regex
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
 }
 function touch($file) {
-        "" | Out-File $file -Encoding ASCII
+    "" | Out-File $file -Encoding ASCII
 }
 function df {
-        get-volume
+    get-volume
 }
-function sed($file, $find, $replace){
-        (Get-Content $file).replace("$find", $replace) | Set-Content $file
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
 }
 function which($name) {
-        Get-Command $name | Select-Object -ExpandProperty Definition
+    Get-Command $name | Select-Object -ExpandProperty Definition
 }
 function export($name, $value) {
-        set-item -force -path "env:$name" -value $value;
+    set-item -force -path "env:$name" -value $value;
 }
 function pkill($name) {
-        ps $name -ErrorAction SilentlyContinue | kill
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
 }
 function pgrep($name) {
-        ps $name
+    Get-Process $name
 }
 
 
 ## Final Line to set prompt
-oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
