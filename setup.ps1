b3e7bcac3086996366e09f4740a0bf6b82b71d48
unknown <solomoncyj@gmail.com>
2023-09-28 18:52:40 +0800

    inform new users

--- setup.ps1
+++ setup.ps1
@@ -15,6 +15,9 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
 
         Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
         Write-Host "The profile @ [$PROFILE] has been created."
+        write-host "if you want to add any persistent components, please do so at
+        [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile 
+        which uses the hash to update the profile and will lead to loss of changes"
     }
     catch {
         throw $_.Exception.Message
