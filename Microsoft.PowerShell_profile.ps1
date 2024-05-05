753a3df44378afdab6dac0d6e63e4b7e45a4edfc
starjunkie-astro <164531747+starjunkie-astro@users.noreply.github.com>
2024-05-05 13:18:10 -0500

    Update Microsoft.PowerShell_profile.ps1
    removed some $EDITOR elseif statements

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -87,9 +87,6 @@ function Test-CommandExists {
 
 # Editor Configuration
 $EDITOR = if (Test-CommandExists nvim) { 'nvim' }
-          elseif (Test-CommandExists pvim) { 'pvim' }
-          elseif (Test-CommandExists vim) { 'vim' }
-          elseif (Test-CommandExists vi) { 'vi' }
           elseif (Test-CommandExists code) { 'code' }
           elseif (Test-CommandExists notepad++) { 'notepad++' }
           elseif (Test-CommandExists sublime_text) { 'sublime_text' }
