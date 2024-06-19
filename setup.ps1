a9ae492e74c1e504363db069fa945b7f18462bfb
solomoncyj <solomoncyj@gmail.com>
2024-06-19 20:24:00 +0800

    set up cross platform for setup script

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,19 @@
+
+
 # Ensure the script can run with elevated privileges
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+$isadmin =
+if ($IsWindows) {
+    ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+elseif (Get-Command touch -ErrorAction Ignore) {
+    touch /tmp *> $null
+    Remove-Item /tmp -ErrorAction Ignore
+    $LASTEXITCODE -eq 0
+}
+if (!$isadmin) {
     Write-Warning "Please run this script as an Administrator!"
     break
 }
-
 # Function to test internet connectivity
 function Test-InternetConnection {
     try {
@@ -22,24 +32,15 @@ if (-not (Test-InternetConnection)) {
 }
 
 # Profile creation or update
-if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
+if (!(Test-Path -Path $PROFILE.CurrentUserCurrentHost -PathType Leaf)) {
     try {
-        # Detect Version of PowerShell & Create Profile directories if they do not exist.
-        $profilePath = ""
-        if ($PSVersionTable.PSEdition -eq "Core") { 
-            $profilePath = "$env:userprofile\Documents\Powershell"
-        }
-        elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-            $profilePath = "$env:userprofile\Documents\WindowsPowerShell"
-        }
-
-        if (!(Test-Path -Path $profilePath)) {
-            New-Item -Path $profilePath -ItemType "directory"
-        }
+        # Create Profile directories if they do not exist.
+        $profilePath = $PROFILE.CurrentUserCurrentHost
+        New-Item -ItemType File -Force -Name $PROFILE.CurrentUserCurrentHost
 
         Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-        Write-Host "The profile @ [$PROFILE] has been created."
-        Write-Host "If you want to add any persistent components, please do so at [$profilePath\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
+        Write-Host "The profile @ [$($PROFILE.CurrentUserCurrentHost)] has been created."
+        Write-Host "If you want to add any persistent components, please do so at [$($PROFILE.CurrentUserAllHosts)] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
     }
     catch {
         Write-Error "Failed to create or update the profile. Error: $_"
@@ -47,16 +48,17 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
 }
 else {
     try {
-        Get-Item -Path $PROFILE | Move-Item -Destination "oldprofile.ps1" -Force
+        Get-Item -Path $PROFILE.CurrentUserCurrentHost | Move-Item -Destination "oldprofile.ps1" -Force
         Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-        Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
-        Write-Host "Please back up any persistent components of your old profile to [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
+        Write-Host "The profile @ [$($PROFILE.CurrentUserCurrentHost)] has been created and old profile moved to ./oldprofile.ps1 ."
+        Write-Host "Please back up any persistent components of your old profile to [$($PROFILE.CurrentUserAllHosts)] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
     }
     catch {
         Write-Error "Failed to backup and update the profile. Error: $_"
     }
 }
-
+#windows 
+if($IsWindows) {
 # OMP Install
 try {
     winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
@@ -108,14 +110,6 @@ try {
 catch {
     Write-Error "Failed to install Chocolatey. Error: $_"
 }
-
-# Terminal Icons Install
-try {
-    Install-Module -Name Terminal-Icons -Repository PSGallery -Force
-}
-catch {
-    Write-Error "Failed to install Terminal Icons module. Error: $_"
-}
 # zoxide Install
 try {
     winget install -e --id ajeetdsouza.zoxide
@@ -123,4 +117,30 @@ try {
 }
 catch {
     Write-Error "Failed to install zoxide. Error: $_"
+}
+}elseif ($IsLinux) {
+    # OMP Install
+    & "curl -s https://ohmyposh.dev/install.sh | bash -s"
+    # zoxide Install
+    & "curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh"
+    #font install
+    mkdir ~/.local/share/fonts -Force
+    Invoke-RestMethod -Uri "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip" -OutFile ".\CascadiaCode.zip"
+    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode"
+    Copy-Item -Path ".\CascadiaCode\*" -Destination "~/.local/share/fonts" -Recurse -Force
+    Remove-Item -Path ".\CascadiaCode\" ".\CascadiaCode.zip" -Force -Recurse
+}elseif ($IsMacOS) {
+    & "brew install jandedobbeleer/oh-my-posh/oh-my-posh zoxide"
+    mkdir ~/.local/share/fonts -Force
+    Invoke-RestMethod -Uri "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip" -OutFile ".\CascadiaCode.zip"
+    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode"
+    Copy-Item -Path ".\CascadiaCode\*" -Destination "/Library/Fonts" -Recurse -Force
+    Remove-Item -Path ".\CascadiaCode\" ".\CascadiaCode.zip" -Force -Recurse
+}
+# Terminal Icons Install
+try {
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+}
+catch {
+    Write-Error "Failed to install Terminal Icons module. Error: $_"
 }
