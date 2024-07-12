de569fc7335408c0e2bdf879ac45a66000ba85dc
danteRub <pablo.mateospe@gmail.com>
2024-07-12 16:46:33 +0200

    change theme  default tokyonight_storm

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -302,7 +302,7 @@ function Get-Theme {
             return
         }
     } else {
-        oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
+        oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/tokyonight_storm.omp.json | Invoke-Expression
     }
 }
 
