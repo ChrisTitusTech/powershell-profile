2296c53bf8524b03eb4ad8c6f9702582189a8eed
danteRub <142012871+danteRub@users.noreply.github.com>
2024-07-14 17:49:36 +0200

    change cd to eza, change shortcut  ls and la

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -246,8 +246,8 @@ function ep { vim $PROFILE }
 function k9 { Stop-Process -Name $args[0] }
 
 # Enhanced Listing
-function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
-function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
+function la { eza -al --color=always | Format-Table -AutoSize }
+function ll { eza -al --color=always | Format-Table -AutoSize }
 
 # Git Shortcuts
 function gs { git status }
