e338c1cac2dd578fad2f0aad86e12bf1ecade205
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-08 01:24:35 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
+# Chris Titus Tech's PowerShell profile
+# Uses oh-my-posh for nice looking ui and has custom history + custom build-in functions
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
 Invoke-Expression (oh-my-posh init pwsh --config $Home\cobalt2.omp.json)
 
 Write-Host "$($PSStyle.Foreground.Yellow)Use 'Show-Help' to list all available functions$($PSStyle.Reset)"
