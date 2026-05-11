43ce642c24faca2f78da61dce3e4262dad39ac01
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-10 23:18:16 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -14,9 +14,6 @@ if (Test-Path $Profile) {
     New-Item -Path $Profile -Force | Out-Null
 }
 
-# Disable telemetry for funziezs
-[Environment]::SetEnvironmentVariable("POWERSHELL_TELEMETRY_OPTOUT", "1", "Machine")
-
 Invoke-WebRequest -Uri https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $Profile
 Invoke-WebRequest -Uri https://github.com/JanDeDobbeleer/oh-my-posh/raw/main/themes/cobalt2.omp.json -OutFile (Split-Path $Profile)
 
