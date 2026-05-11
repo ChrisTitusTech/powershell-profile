9e4063175b51439959287833d6b744d0402d7534
Gabi <218829269+GabiNun2@users.noreply.github.com>
2026-05-11 06:41:08 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -11,6 +11,9 @@ if (Test-Path $Profile) {
     New-Item -Path $Profile -Force | Out-Null
 }
 
+# Disable pwsh telemetry for funnzies :)
+[System.Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT','1','Machine')
+
 Invoke-WebRequest -Uri https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $Profile
 Invoke-WebRequest -Uri https://github.com/JanDeDobbeleer/oh-my-posh/raw/main/themes/cobalt2.omp.json -OutFile (Split-Path $Profile)
 
