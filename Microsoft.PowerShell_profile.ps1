59f643f826a8eabe08a8241a53474c7c4c70a556
Bernhard Sadransky <11773352+besa2042@users.noreply.github.com>
2026-08-05 11:54:56 +0200

    [Microsoft.PowerShell_profile.ps1] clear vim alias if present and check for vim app directly

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -289,8 +289,12 @@ function Resolve-Editor {
 Initialize-OptionalModule
 
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (Get-Alias vim -ErrorAction SilentlyContinue) {
+    Remove-Alias -Name vim
+}
 $EDITOR = Resolve-Editor
-if ($EDITOR -ne "vim") {
+if (-not (Get-Command -Name vim -CommandType Application -ErrorAction SilentlyContinue)) {
     Set-Alias -Name vim -Value $EDITOR -Force
 }
 
